eec0ab8573e5e3efb04a775146c7529bab7495e8
Malavika <malavikavinod99@gmail.com>
2023-05-07 12:10:28 +0530

    Regression project

---
+++ README.md
@@ -0,0 +1,14 @@
+## End to end ML project
+
+### Created an environment
+
+```
+conda create -p venv python=3.8
+
+```
+
+### Install all required libraries 
+
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements= []
+    hyphen_e_dot = '-e .'
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+
+setup(
+    name = 'Regression Project',
+    version = '0.0.1',
+    author = 'Malavika Vinod',
+    author_email = 'malavikavinod99@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
